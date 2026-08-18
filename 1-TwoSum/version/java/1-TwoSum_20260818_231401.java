e4412a02e22960f000040c001d5bcd4f448dd997
Geethika <geethikach.dev@gmail.com>
2026-08-18 23:14:01 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/java/1-TwoSum_20260818_231401.java
@@ -0,0 +1,15 @@
+// Last updated: 18/08/2026, 23:14:01
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+for(int left=1;left<n;left++){
+        for(int right=left;right<n;right++){
+            int sum=nums[right-left]+nums[right];
+            if(sum==target){
+                return new int[]{right-left,right};
+            }
+        }
+    }
+    return new int[]{};
+}
+}
